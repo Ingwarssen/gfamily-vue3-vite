eb14efecade1ede9418489d129a3cc2f93cd882d
igor <13en4wow@gmail.com>
2021-05-23 17:41:45 +0300

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,32 @@
 <template>
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <p>
+    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+  <div>
+    <h2>Filter LearnVue Articles</h2>
+    <input type="text" placeholder="Filter Search" v-model="query" />
+    {{ query }}
+    <button @click="reset">Reset</button>
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
+  setup() {
+    const query = ref('');
+
+    const reset = (evt) => {
+      query.value = ''; // clears the query
+    };
+
     return {
-      count: 0
-    }
-  }
-}
+      reset,
+      query,
+    };
+  },
+};
 </script>
